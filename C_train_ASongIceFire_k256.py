69198fd8f1fd9e3d82b0a67cf67a2ad8ca27eae9
AnonSimRank <129581948+AnonSimRank@users.noreply.github.com>
2024-01-03 16:49:43 +0800

    Update C_train_ASongIceFire_k256.py

--- C_train_ASongIceFire_k256.py
+++ C_train_ASongIceFire_k256.py
@@ -64,15 +64,6 @@ print("precision:", precision_score)
 precision_score_file = save_file_dir + "/" + "precision_score.txt"
 np.savetxt(precision_score_file, np.array([precision_score]))
 
-recall_score = Metrics.recall(train_data, test_data, pred_data)
-print("recall:", recall_score)
-recall_score_file = save_file_dir + "/" + "recall_score.txt"
-np.savetxt(recall_score_file, np.array([recall_score]))
-
-f_score = Metrics.F_score(precision_score, recall_score)
-print("f_score:", f_score)
-f_score_file = save_file_dir + "/" + "f_score.txt"
-np.savetxt(f_score_file, np.array([f_score]))
 
 auc_score = Metrics.AUC(train_data, test_data, pred_data, n=1000)
 print("auc_score:", auc_score)
